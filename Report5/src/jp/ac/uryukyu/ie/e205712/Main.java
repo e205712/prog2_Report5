b035995adf1c7ed0f04cc4895ac948cb447dc156
e205712 <75283789+e205712@users.noreply.github.com>
2022-11-29 17:34:36 +0900

    step3 files
    Step3 files were uploaded.

--- Report5/src/jp/ac/uryukyu/ie/e205712/Main.java
+++ Report5/src/jp/ac/uryukyu/ie/e205712/Main.java
@@ -1,21 +1,10 @@
 package jp.ac.uryukyu.ie.e205712;
 
-import java.io.FileWriter;
-import java.io.IOException;
-
 public class Main {
     public static void main(String[] args){
-        try{
-            //nullを変数strに代入
-            String str = null;
-            //変数が"null"なら4が出力されるが今はnullなので、、
-            System.out.println(str.length());
-        }catch(NullPointerException e){
-            //nullがcatchされるとここの処理を行う
-            //println()に自由に文字を入れればメッセージを自由に出力可能 
-            System.out.println("ぬるぽえらー");
-            //e.getMessage()で定型のエラーメッセージを出力
-            System.out.println(e.getMessage());
-        }
+        //nullを変数strに代入
+        String str = "壱百満";
+        //変数が"null"なら4が出力されるが今はnullなので、、
+        int value = Integer.parseInt(str);
     }
 }
