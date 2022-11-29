f4ce7b397e4263a42975cd947bf30850595420a2
e205712 <75283789+e205712@users.noreply.github.com>
2022-11-29 17:03:19 +0900

    step1 files
    Step1 files were uploaded.

---
+++ Report5/src/jp/ac/uryukyu/ie/e205712/Main.java
@@ -0,0 +1,10 @@
+package jp.ac.uryukyu.ie.e205712;
+
+public class Main {
+    public static void main(String[] args){
+        //nullを変数strに代入
+        String str = null;
+        //変数が"null"なら4が出力されるが今はnullなので、、
+        System.out.println(str.length());
+    }
+}
